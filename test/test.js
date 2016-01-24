daf1cf00df608ea24fa04fa8d98a6172a2f843fc
Eeo Jun <packwolf58@gmail.com>
2016-01-24 19:20:49 +0800

    Moved tests to test.js instead of inlining

---
+++ test/test.js
@@ -0,0 +1,36 @@
+describe('nut', function() {
+  var $ = nut;
+  var assert = chai.assert;
+
+  it('returns a one element array if it is an id query', function() {
+    assert.deepEqual($('#nut'), document.getElementById('nut'));
+  });
+  it('supports classes', function() {
+    assert.deepEqual(
+      $('.nut'),
+      [].slice.call($('#nut').children)
+    );
+  });
+  it('supports selectors', function() {
+    assert.deepEqual(
+      $('#nut > span'),
+      $('.nut')
+    );
+    assert.deepEqual(
+      $('span.nut'),
+      $('.nut')
+    );
+  });
+  it('supports a context element', function() {
+    assert.deepEqual(
+      $('span', $('#nut')),
+      $('.nut')
+    );
+  });
+  it('supports a context string', function() {
+    assert.deepEqual(
+      $('span', 'div#nut'),
+      $('.nut')
+    );
+  });
+});
