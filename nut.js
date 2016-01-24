21c41abcf680fe45527046cf8e79617f3ab54027
Eeo Jun <packwolf58@gmail.com>
2016-01-24 23:11:04 +0800

    Compress better

--- nut.js
+++ nut.js
@@ -1,22 +1,20 @@
-!function() {
-  function _(ctx) {
-    return typeof ctx == 'string'
-      ? nut.el(ctx)
-      : ctx || document;
-  };
+nut = function(s, ctx) {
+  ctx = nut._(ctx);
+  return [].slice.call(
+    /^\.[\w\-]+$/.test(s)
+      ? ctx.getElementsByClassName(s.slice(1))
+      : ctx.querySelectorAll(s));
+};
 
-  nut = function(s, ctx) {
-    ctx = _(ctx);
-    return [].slice.call(
-      /^\.[\w\-]+$/.test(s)
-        ? ctx.getElementsByClassName(s.slice(1))
-        : ctx.querySelectorAll(s));
-  };
+nut.el = function(s, ctx) {
+  ctx = nut._(ctx);
+  return /^#[\w\-]+$/.test(s)
+    ? ctx.getElementById(s.slice(1))
+    : ctx.querySelector(s);
+};
 
-  nut.el = function(s, ctx) {
-    ctx = _(ctx);
-    return /^#[\w\-]+$/.test(s)
-      ? ctx.getElementById(s.slice(1))
-      : ctx.querySelector(s);
-  };
-}();
+nut._ = function(ctx) {
+  return typeof ctx == 'string'
+    ? nut.el(ctx)
+    : ctx || document;
+};
