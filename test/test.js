1fc93d6f70d26436584769df68aaf0fadc466b24
Eeo Jun <packwolf58@gmail.com>
2016-05-04 19:37:56 +0800

    Add more comprehensive tests

--- test/test.js
+++ test/test.js
@@ -1,67 +1,69 @@
-describe('nut', function() {
-  var $ = nut;
-  var assert = chai.assert;
+var $ = nut;
+var assert = chai.assert;
+var qs  = document.querySelector.bind(document);
+var qsa = document.querySelectorAll.bind(document);
+var toa = [].slice;
 
-  it('supports classes', function() {
-    assert.deepEqual(
-      $('.nut'),
-      [].slice.call($.el('#nut').children)
-    );
-  });
-  it('supports selectors', function() {
-    assert.deepEqual(
-      $('#nut > span'),
-      $('.nut')
-    );
-    assert.deepEqual(
-      $('span.nut'),
-      $('.nut')
-    );
-  });
-  it('supports a context element', function() {
-    assert.deepEqual(
-      $('span', $.el('#nut')),
-      $('.nut')
-    );
-  });
-  it('supports a context string', function() {
-    assert.deepEqual(
-      $('span', 'div#nut'),
-      $('.nut')
-    );
-  });
-});
 
 describe('nut.el', function() {
-  var $ = nut;
-  var assert = chai.assert;
-  var qs = document.querySelector.bind(document);
+  it('supports id queries', function() {
+    assert.equal(
+      $.el('#nut'),
+      qs('#nut')
+    );
+  });
 
-  it('returns the first element matched', function() {
+  it('supports selectors', function() {
     assert.equal(
       $.el('.nut'),
       qs('.nut')
     );
   });
 
-  it('supports selectors', function() {
+  it('accepts element as selector argument', function() {
     assert.equal(
-      $.el('div#nut'),
+      $.el(qs('#nut')),
       qs('#nut')
     );
   });
 
-  it('supports context', function() {
-    assert.equal(
-      $.el('#nut', qs('body')),
-      $.el('body > #nut')
+  it('supports context argument', function() {
+    var div = qs('#nut');
+    var span = qs('#nut .nut');
+    assert.equal($.el('.nut', '#nut'), span);
+    assert.equal($.el('.nut', div), span);
+  });
+});
+
+
+describe('nut', function() {
+  it('supports class queries', function() {
+    assert.deepEqual(
+      $('.nut'),
+      toa.call(qsa('.nut'))
     );
   });
 
-  it('supports context string', function() {
-    assert.equal(
-      $.el('#nut', 'body'),
-      $.el('body > #nut')
+  it('supports selectors', function() {
+    assert.deepEqual(
+      $('#nut .nut'),
+      toa.call(qs('#nut').children)
     );
+  });
+
+  describe('context argument', function() {
+    it('can be a string', function() {
+      assert.deepEqual(
+        $('.nut', '#nut'),
+        toa.call(qs('#nut').children)
+      );
+    });
+
+    it('can be an element', function() {
+      assert.deepEqual(
+        $('*', qs('#nut')),
+        toa.call(qs('#nut').children)
+      );
+    });
   });
 });
