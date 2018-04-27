50bb19d922df9ec86f06dc84aa78186dab905751
Eeo Jun <141bytes@gmail.com>
2018-04-27 20:43:10 +0100

    minor update

--- nut.js
+++ nut.js
@@ -1,17 +1,16 @@
 nut = function(s, ctx) {
-  ctx = nut.el(ctx);
-  return [].slice.call(
-    /^\.[\w\-]+$/.test(s)
-      ? ctx.getElementsByClassName(s.slice(1))
-      : ctx.querySelectorAll(s));
+    ctx = nut.el(ctx);
+    return [].slice.call(
+        /^\.[\w\-]+$/.test(s)
+            ? ctx.getElementsByClassName(s.slice(1))
+            : ctx.querySelectorAll(s));
 };
 
 nut.el = function(s, ctx) {
-  if (typeof s != 'string') {
-    return s || document;
-  }
-  ctx = nut.el(ctx);
-  return /^#[\w\-]+$/.test(s) && ctx.getElementById
-    ? ctx.getElementById(s.slice(1))
-    : ctx.querySelector(s);
+    if (typeof s !== 'string') {
+        return s || document;
+    }
+    return (/^#[\w\-]+$/.test(s))
+        ? document.getElementById(s.slice(1))
+        : nut.el(ctx).querySelector(s);
 };
